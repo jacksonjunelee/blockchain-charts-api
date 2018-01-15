3b72ac1071a090a26ab66fee4448b39a9a5a1d72
jacksonjunelee <jacksonjunelee@gmail.com>
2018-01-14 22:10:13 -0500

    added logic container

---
+++ src/app/components/blockchain-chart.container.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'blockchain-chart-container',
+  templateUrl: './blockchain-chart.container.component.html',
+  styleUrls: ['./blockchain-chart.container.component.css']
+})
+export class BlockchainChartsContainerComponent {
+  title = 'app';
+}
